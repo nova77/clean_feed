a7c8782445414fe5f87adcf583963b412b47c3ca
Norman <nova77@gmail.com>
2021-12-11 18:16:31 +0000

    now supports merging

--- clean_feed.py
+++ clean_feed.py
@@ -1,20 +1,32 @@
 # Clean XML feed(s).
 # Usage:
-#  python clean_feed.py <feed_url>
+#  python clean_feed.py feed_url [feed_url ...]
 
 import sys
 import requests
 from bs4 import BeautifulSoup
+from xml.etree import ElementTree
 
 
-def fetch_clean_save(feed_url):
-  r = requests.get(feed_url)
-  soup = BeautifulSoup(r.text,'xml') 
-  print (soup.prettify()) 
+def _read_url_as_xml(url):
+  r = requests.get(url)
+  soup = BeautifulSoup(r.text, 'xml')
+  return ElementTree.fromstring(soup.prettify())
 
 
-if __name__== "__main__":
+def fetch_clean_save(feed_urls):
+  merged_feed = None
+  for feed_url in feed_urls:
+    feed = _read_url_as_xml(feed_url)
+    if merged_feed is None:
+      merged_feed = feed
+    else:
+      merged_feed.extend(feed)
+  print(ElementTree.tostring(merged_feed, encoding='unicode'))
+
+
+if __name__ == "__main__":
   if len(sys.argv) < 2:
-    print ("Usage: clean_feed.py feed")
+    print("Usage: clean_feed.py feed_url [feed_url ...]")
   else:
-    fetch_clean_save(sys.argv[1])
+    fetch_clean_save(sys.argv[1:])
